9d7a95a42d9a18c61bbbd401e51ece89e700bb44
JuuzouMarques <miltonneto2002@gmail.com>
2022-01-29 17:36:55 -0300

    Primeiros tratamentos com variáveis

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,8 +1,24 @@
 package src;
 
+import java.util.Locale;
+
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");   
+        Locale.setDefault(Locale.US);
+        
+        System.out.println("Olá, mundo!");
+        int y = 32;
+        double x = 45.311646;
+        double result = y + x;
+        System.out.println(y);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
+        System.out.printf("A soma de " + y + " com %.2f resulta em: %.2f%n", x, result);
+
+        String nome = "Maria";
+        int idade = 25;
+        double salario = 2500.00;
+        System.out.printf("%s tem %d anos, e salário de %.2f reais %n", nome, idade, salario);
     }
 }
