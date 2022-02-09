1c9304c0c12add69dba463fd6b0574e81889ecb6
JuuzouMarques <miltonneto2002@gmail.com>
2022-02-09 00:44:34 -0300

    Exercícios Estruturas de Repetição

---
+++ curso_programacao/exercicios/exercicios05.java
@@ -0,0 +1,33 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicios05 {
+    // Exercícios com for
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Exercício 01
+        int num = sc.nextInt();
+        if (num >= 1 && num <= 1000) {
+            for (int i = 1; i <= num; i += 2) {
+                System.out.println(i);
+            }
+        }
+
+        // Exercício 02
+        int n = sc.nextInt();
+        int in = 0, out = 0;
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            if (num >= 10 && num <= 20) {
+                in++;
+            } else {
+                out++;
+            }
+        }
+        System.out.printf("%d in %n%d out", in, out);
+
+        sc.close();
+    }
+}
